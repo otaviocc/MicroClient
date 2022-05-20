46cc459b534c9c600981834fa56342ad8be9f9de
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2022-05-20 17:57:45 +0200

    Add network status

--- Sources/MicroClient/NetworkClient.swift
+++ Sources/MicroClient/NetworkClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 /// The network client interface.
 public protocol NetworkClientProtocol {
@@ -18,12 +19,26 @@ public protocol NetworkClientProtocol {
     ) async throws -> NetworkResponse<ResponseModel>
 }
 
+/// The network client status provider.
+public protocol NetworkClientStatusProvider {
+
+    /// Publishes the network client status.
+    ///
+    /// There are two states, `.running` and `.idle` and they represent
+    /// if the network client is perform a network request or not.
+    ///
+    /// - Returns: The status publisher.
+    func statusPublisher(
+    ) -> AnyPublisher<NetworkClientStatus, Never>
+}
+
 /// The network client, conforming to the `NetworkClientProtocol` protocol.
-public final class NetworkClient: NetworkClientProtocol {
+public final class NetworkClient: NetworkClientProtocol, NetworkClientStatusProvider {
 
     // MARK: - Properties
 
     private let configuration: NetworkConfiguration
+    private let statusSubject = PassthroughSubject<NetworkClientStatus, Never>()
 
     // MARK: - Life cycle
 
@@ -38,6 +53,12 @@ public final class NetworkClient: NetworkClientProtocol {
     public func run<RequestModel, ResponseModel>(
         _ networkRequest: NetworkRequest<RequestModel, ResponseModel>
     ) async throws -> NetworkResponse<ResponseModel> {
+        statusSubject.send(.running)
+
+        defer {
+            statusSubject.send(.idle)
+        }
+
         var request = try URLRequest.makeURLRequest(
             configuration: configuration,
             networkRequest: networkRequest
@@ -58,5 +79,10 @@ public final class NetworkClient: NetworkClientProtocol {
             ),
             response: response
         )
+    }
+
+    public func statusPublisher(
+    ) -> AnyPublisher<NetworkClientStatus, Never> {
+        statusSubject.eraseToAnyPublisher()
     }
 }
